dc8d6e97d4cb61b8d9180bf0065290a52d319982
qqoj <qq.ojala@gmail.com>
2016-03-25 20:28:22 +0200

    Search for blueprints

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,5 +1,15 @@
 class IndexController < ApplicationController
   def index
-    @blueprints = Blueprint.all
+  end
+
+  def search
+    @blueprints = params[:value]
+                      .lines
+                      .map(&:strip)
+                      .map { |line| EveName.where(value: line).to_a }
+                      .flatten
+                      .map { |name| Blueprint.find_by type_id: name.type_id }
+                      .uniq
+    render :index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   scope format: false do
     root 'index#index'
+    post '/search', to: 'index#search'
   end
 
 end
