c7f1366f5458ee52577616d23e9a249d7a47ef14
qqoj <qq.ojala@gmail.com>
2016-03-25 22:50:11 +0200

    Calculate profits

--- app/models/blueprint.rb
+++ app/models/blueprint.rb
@@ -1,3 +1,13 @@
 class Blueprint < ActiveRecord::Base
   serialize :activities, Hash
+
+  def materials
+    activities.try(:[], 'manufacturing').try(:[], 'materials') || []
+  end
+
+  def cost
+    materials.reduce(0) do |a, m|
+      a + m['quantity'] * EveItem.get(m).try(:price)
+    end
+  end
 end
---
+++ spec/models/blueprint_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe Blueprint, type: :model do
+  describe "materials" do
+    it "can be missing" do
+      expect(build(:blueprint, activities: {}).materials).to eq []
+    end
+
+    it "are found from activities" do
+      expect(build(:blueprint, activities: {
+          manufacturing: {materials: [1, 2, 3]}.stringify_keys
+      }.stringify_keys).materials).to eq [1, 2, 3]
+    end
+  end
+
+  describe "cost" do
+    it "uses materials" do
+      create :eve_item, type_id: 1, price: 1.1
+      create :eve_item, type_id: 2, price: 1.2
+      create :eve_item, type_id: 3, price: 1.3
+
+      blueprint = build(:blueprint)
+      materials = [
+          {typeID: 1, quantity: 2}.stringify_keys,
+          {typeID: 2, quantity: 1}.stringify_keys,
+          {typeID: 3, quantity: 3}.stringify_keys
+      ]
+      allow(blueprint).to receive(:materials).and_return materials
+      expect(blueprint.cost).to eq 7.3
+    end
+  end
+end
