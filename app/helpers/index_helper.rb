04efe92926a39cf52df35d0549d57f54da4eeb9b
qqoj <qq.ojala@gmail.com>
2016-03-26 13:07:21 +0200

    Example search moved to helper

---
+++ app/helpers/index_helper.rb
@@ -0,0 +1,10 @@
+module IndexHelper
+  def example_search
+    [
+        "Carbonized Lead S Blueprint	1	Projectile Ammo Blueprint			0,01 m3",
+        "Proton S Blueprint	1	Projectile Ammo Blueprint			0,01 m3",
+        "Warrior II Blueprint",
+        "Vexor",
+    ].join "\n"
+  end
+end
