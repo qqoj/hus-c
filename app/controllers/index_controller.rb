7ecefa9089e056ca5029813e692e5ebdba169eb9
qqoj <qq.ojala@gmail.com>
2016-03-27 16:09:30 +0300

    Limit searches to 10 blueprints

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -3,16 +3,27 @@ class IndexController < ApplicationController
   end
 
   def search
-    @blueprints = params[:value]
-                      .lines
-                      .map { |l| l.strip.split("\t").first }
-                      .compact
-                      .map { |l| EveItem.where(name: "#{l.gsub(/ Blueprint/, '')} Blueprint").to_a }
-                      .flatten
-                      .map { |i| Blueprint.find_by(type_id: i.type_id) }
-                      .uniq
-                      .each { |b| b.system = cookies[:system] }
-                      .sort_by { |b| -b.per_hour(b.profit) }
-    render :index
+    results = search_by(params[:value])
+    if results.size > 10
+      redirect_to root_url, notice: "Too many blueprints"
+    else
+      @blueprints = results
+                        .each { |b| b.system = cookies[:system] }
+                        .sort_by { |b| -b.per_hour(b.profit) }
+      render :index
+    end
+  end
+
+  private
+
+  def search_by(query)
+    query
+        .lines
+        .map { |l| l.strip.split("\t").first }
+        .compact
+        .map { |l| EveItem.where(name: "#{l.gsub(/ Blueprint/, '')} Blueprint").to_a }
+        .flatten
+        .map { |i| Blueprint.find_by(type_id: i.type_id) }
+        .uniq
   end
 end
