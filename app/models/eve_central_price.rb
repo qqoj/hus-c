27d60eb7ac575c2537d6e6530969af0b2c1b07fd
qqoj <qq.ojala@gmail.com>
2016-03-27 15:25:35 +0300

    Make the cache a bit longer

--- app/models/eve_central_price.rb
+++ app/models/eve_central_price.rb
@@ -1,6 +1,6 @@
 class EveCentralPrice
   def self.get(type_id, system)
-    Rails.cache.fetch "eve-central/#{system}/#{type_id}", expires_in: 1.hour do
+    Rails.cache.fetch "eve-central/#{system}/#{type_id}", expires_in: 8.hours do
       response = JSON.parse(RestClient.get "#{Rails.configuration.eve_central}?typeid=#{type_id}&usesystem=#{system}").first.deep_symbolize_keys!
       {
           buy: response[:buy][:median],
