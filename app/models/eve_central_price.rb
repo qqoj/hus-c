b4c5a9b0d50350ef2b85c0b5ac8999c0a8f21595
qqoj <qq.ojala@gmail.com>
2016-03-27 15:24:06 +0300

    Cache all EVE-Central prices for 1 hour

--- app/models/eve_central_price.rb
+++ app/models/eve_central_price.rb
@@ -1,10 +1,12 @@
 class EveCentralPrice
   def self.get(type_id, system)
-    response = JSON.parse(RestClient.get "#{Rails.configuration.eve_central}?typeid=#{type_id}&usesystem=#{system}").first.deep_symbolize_keys!
-    {
-        buy: response[:buy][:median],
-        sell: response[:sell][:median],
-        all: response[:all][:median],
-    }
+    Rails.cache.fetch "eve-central/#{system}/#{type_id}", expires_in: 1.hour do
+      response = JSON.parse(RestClient.get "#{Rails.configuration.eve_central}?typeid=#{type_id}&usesystem=#{system}").first.deep_symbolize_keys!
+      {
+          buy: response[:buy][:median],
+          sell: response[:sell][:median],
+          all: response[:all][:median],
+      }
+    end
   end
 end
